47dcb744a242b374ff57d4741d8fb38bcee4e6fc
Johncent Ventura <141490897+JohncentVentura@users.noreply.github.com>
2023-12-25 18:47:12 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,25 +1,21 @@
-const homeSection = document.getElementById("id-home");
-//const skillSection = document.getElementById("id-skills");
-const contactSection = document.getElementById("id-contact");
+(function setProperties() {
+    let body = document.body;
 
-function setProperties() {
+    //Setting height of sections
+    const homeSection = document.getElementById("id-home");
+    //const contactSection = document.getElementById("id-contact");
     const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
     const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
 
     homeSection.style.setProperty("height", `${vh}px`);
-    //skillSection.style.setProperty("height", `${vh}px`);
-    contactSection.style.setProperty("height", `${vh}px`);
+    //contactSection.style.setProperty("height", `${vh}px`);
     console.log("vw", vw, "vh", vh);
-}
 
-setProperties();
+    //Change theme (Start with Dark Theme)
+    const themeButton = document.querySelector(".theme-button");
+    themeButton.addEventListener('mousedown', () => {
+        body.classList.toggle('light-theme');
+    });
+})();
 
-function ChangeTheme() {
-    const btnTheme = document.querySelector(".theme-button");
-    btnTheme.addEventListener('mousedown', () => {
-        let element = document.body;
-        element.classList.toggle('light-theme');
-    })
-}
 
-ChangeTheme();
